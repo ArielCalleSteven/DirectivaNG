c5cdb5118ac9090a24823c5a86eca18e9f65f1ad
ArielCalleSteven <a.calleduma123@gmail.com>
2022-12-05 09:11:05 -0500

    Directivas NG: Funcionando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './componentes/home/home.component';
+import { Home2Component } from './componentes/home2/home2.component';
+import { Home3Component } from './componentes/home3/home3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    Home2Component,
+    Home3Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
